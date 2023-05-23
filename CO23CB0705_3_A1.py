45f7d3a263fc78f86b4b46f67abbd0f391dc4448
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-05-23 15:18:24 +0530

    Update CO23CB0705_3_A1.py

--- CO23CB0705_3_A1.py
+++ CO23CB0705_3_A1.py
@@ -1,1 +1,9 @@
-
+countries_and_capitals = {
+        'India': 'New Delhi',
+        'Germany': 'Berlin',
+        'Russia': 'Moscow',
+        'Japan': 'Tokyo',
+        'China': 'Beijing'
+    }
+for country, capital in countries_and_capitals.items():
+    print(f"Country: {country}\tCapital: {capital}")
