21d8ddfc19a90efb479f9bde4d366a8907162dfb
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-05-23 16:02:30 +0530

    Update CO23CB0705_3_AA1.py

--- CO23CB0705_3_AA1.py
+++ CO23CB0705_3_AA1.py
@@ -1,1 +1,18 @@
+states_and_capitals = {
+    'UP': 'Lucknow',
+    'Bihar': 'Patna',
+    'Rajasthan': 'Udaipur',
+    'Chattisgarh': 'Raipur',
+    'Jharkhand': 'Ranchi'
+}
 
+for state, capital in states_and_capitals.items():
+    print(f"State: {state}\tCapital: {capital}")
+
+# Correcting the capital of Rajasthan
+states_and_capitals['Rajasthan'] = 'Jaipur'
+
+print("\n")
+
+for state, capital in states_and_capitals.items():
+    print(f"State: {state}\tCapital: {capital}")
