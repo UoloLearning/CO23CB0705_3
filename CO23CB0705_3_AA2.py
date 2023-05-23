26253854718fde65c1638bcfae0fb14f2e6dac19
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-05-23 16:24:30 +0530

    Update CO23CB0705_3_AA2.py

--- CO23CB0705_3_AA2.py
+++ CO23CB0705_3_AA2.py
@@ -1,1 +1,16 @@
+# Create a dictionary to store student marks
+student_result = {
+    'Maths': 89,
+    'Science': 87,
+    'English': 93,
+    'Hindi': 97,
+    'Computer Science': 96
+}
 
+# Display student details
+print("Student Result:")
+print("Maths:", student_result['Maths'])
+print("Science:", student_result['Science'])
+print("English:", student_result['English'])
+print("Hindi:", student_result['Hindi'])
+print("Computer Science:", student_result['Computer Science'])
